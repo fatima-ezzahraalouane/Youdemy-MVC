7340869b4ae6b6cc700eef8f680f3526aa239a73
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-04 17:01:58 +0100

    modifier le code de connexion avec database de mysql a postgresql

---
+++ BackEnd/config/Database.php
@@ -0,0 +1,35 @@
+<?php 
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'youdemyMvc';
+    private $username = 'postgres';
+    private $password = '1234567890';
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "pgsql:host={$this->host};dbname={$this->db_name};",
+                $this->username,
+                password: $this->password
+            );
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // echo "Connexion à la base de données réussie !";
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+        return $this->conn;
+    }
+
+    public function closeConnection() {
+        $this->conn = null;
+    }
+}
+
+
+
+$j = (new Database())->getConnection();
+// echo "dd";
+?>
