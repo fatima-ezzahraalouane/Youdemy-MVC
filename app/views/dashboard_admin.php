6573ee5d0a14257c754b184d76fd0270bd7de535
fatima-ezzahraalouane <falouane38@gmail.com>
2025-02-05 11:16:56 +0100

    creer la structure mvc

--- app/views/dashboard_admin.php
+++ app/views/dashboard_admin.php
@@ -52,6 +52,8 @@ $tags = $admin->afficherTags();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../assets/images/youdemy2.png" type="image/x-icon" />
+	<link rel="shortcut icon" type="image/x-icon" href="../assets/images/youdemy2.png" />
     <title>Dashboard Administrateur</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
